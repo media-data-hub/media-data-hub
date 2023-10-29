42b1871ded5d07b1e3e2cd4066dfdb000082aeee
Joshua Avalon <git@avalon.sh>
2023-10-29 19:00:24 +0800

    chore: fix lint

--- pb_migrations/1698576963_updated_collection.js
+++ pb_migrations/1698576963_updated_collection.js
@@ -1,37 +1,38 @@
 /// <reference path="../pb_data/types.d.ts" />
-migrate((db) => {
-  const dao = new Dao(db)
-  const collection = dao.findCollectionByNameOrId("dwstht72so8d0d6")
+migrate(
+  db => {
+    const dao = new Dao(db);
+    const collection = dao.findCollectionByNameOrId("dwstht72so8d0d6");
 
-  // add
-  collection.schema.addField(new SchemaField({
-    "system": false,
-    "id": "lbux6exr",
-    "name": "thumbnails",
-    "type": "file",
-    "required": false,
-    "presentable": false,
-    "unique": false,
-    "options": {
-      "maxSelect": 99,
-      "maxSize": 5242880,
-      "mimeTypes": [
-        "image/jpeg",
-        "image/png",
-        "image/webp"
-      ],
-      "thumbs": [],
-      "protected": false
-    }
-  }))
+    // add
+    collection.schema.addField(
+      new SchemaField({
+        system: false,
+        id: "lbux6exr",
+        name: "thumbnails",
+        type: "file",
+        required: false,
+        presentable: false,
+        unique: false,
+        options: {
+          maxSelect: 99,
+          maxSize: 5242880,
+          mimeTypes: ["image/jpeg", "image/png", "image/webp"],
+          thumbs: [],
+          protected: false
+        }
+      })
+    );
 
-  return dao.saveCollection(collection)
-}, (db) => {
-  const dao = new Dao(db)
-  const collection = dao.findCollectionByNameOrId("dwstht72so8d0d6")
+    return dao.saveCollection(collection);
+  },
+  db => {
+    const dao = new Dao(db);
+    const collection = dao.findCollectionByNameOrId("dwstht72so8d0d6");
 
-  // remove
-  collection.schema.removeField("lbux6exr")
+    // remove
+    collection.schema.removeField("lbux6exr");
 
-  return dao.saveCollection(collection)
-})
+    return dao.saveCollection(collection);
+  }
+);
