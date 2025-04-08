2222502445ae6029d0345f35a09c7a1904dc4df0
Joshua Avalon <git@avalon.sh>
2025-04-08 10:11:17 +0800

    feat: Add index to role

---
+++ pb_migrations/1744078091_updated_role.js
@@ -0,0 +1,20 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate(app => {
+  const collection = app.findCollectionByNameOrId("0d22acftfgyyqh8");
+
+  // update collection data
+  unmarshal({
+    indexes: [
+      "CREATE INDEX `idx_oHUy9oGMXr` ON `role` (`name`)"
+    ]
+  }, collection);
+
+  return app.save(collection);
+}, app => {
+  const collection = app.findCollectionByNameOrId("0d22acftfgyyqh8");
+
+  // update collection data
+  unmarshal({ indexes: [] }, collection);
+
+  return app.save(collection);
+});
